566d7ef94c03f9a2b622b739cce9bacb91e06a20
NEORIS\hassil.pina <hassil.pina@neoris.com>
2021-03-17 20:29:10 -0700

    se agrego CreateOrderTest

--- page_model/test/CreateOrderTest.js
+++ page_model/test/CreateOrderTest.js
@@ -1,6 +1,7 @@
 import CartPage from '../pages/CartPage'
 import InformationPage from '../pages/InformationPage'
 import LoginPage from '../pages/LoginPage'
+import OverviewPage from '../pages/OverviewPage'
 import ProductsPage from '../pages/ProductsPage'
 
 fixture('Create Order')
@@ -23,4 +24,28 @@ test('Continue with missing mail information', async t => {
         await t.expect(InformationPage.msgFailedNameRequerid.exists).ok()
         await t.expect(InformationPage.msgFailedNameRequerid.innerText).eql('Error: First Name is required')
 
+})
+
+test('Complete a purchase', async t => {
+    await t
+        .typeText(LoginPage.txtUsername,'standard_user')
+        .typeText(LoginPage.txtPassword,'secret_sauce')
+        .click(LoginPage.btnLogin)
+        await t.expect(ProductsPage.labelProducts.exists).ok()
+        .click(ProductsPage.btnAddToCart)
+        await t.expect(ProductsPage.btnRemoveProduct.exists).ok()
+        .click(ProductsPage.imgCart)
+        await t.expect(CartPage.btnCheckout.exists).ok()
+        .click(CartPage.btnCheckout)
+        await t.expect(InformationPage.labelYourInformation.exists).ok()
+        .typeText(InformationPage.inputName,'Hassil')
+        .typeText(InformationPage.inputLastName,'Medina')
+        .typeText(InformationPage.inputPostalCode,'8000')
+        .click(InformationPage.btnContinue)
+        await t.expect(OverviewPage.labelOverview.exists).ok()
+        await t.expect(OverviewPage.labelOverview.innerText).eql('Checkout: Overview')
+        await t.expect(OverviewPage.labelPaymentInformation.exists).ok()
+        .click(OverviewPage.btnFinish)
+        await t.expect(OverviewPage.msgSucces.exists).ok()
+        await t.expect(OverviewPage.msgSucces.innerText).eql('THANK YOU FOR YOUR ORDER')
 })
