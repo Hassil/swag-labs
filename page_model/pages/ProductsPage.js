b319b72a0fd3c5d6de58fbbd3c0a329d04dbda29
NEORIS\hassil.pina <hassil.pina@neoris.com>
2021-03-17 18:51:50 -0700

    imprimiendo elementos

--- page_model/pages/ProductsPage.js
+++ page_model/pages/ProductsPage.js
@@ -6,6 +6,8 @@ class ProductsPage {
         this.btnMenu = Selector('#react-burger-menu-btn')
         this.linkLogout = Selector('#logout_sidebar_link')
         this.imgCart = Selector('#shopping_cart_container')
+        this.btnAddToCart = Selector('.btn_primary')
+        this.btnRemoveProduct = Selector('.btn_secondary')
 
     }
 }
---
+++ page_model/test/AddProductCart.js
@@ -0,0 +1,35 @@
+import CartPage from '../pages/CartPage'
+import LoginPage from '../pages/LoginPage'
+import ProductsPage from '../pages/ProductsPage'
+
+fixture('Add product ')
+    .page `https://www.saucedemo.com/`
+
+test('Add one product', async t => {
+    await t
+        .typeText(LoginPage.txtUsername,'standard_user')
+        .typeText(LoginPage.txtPassword,'secret_sauce')
+        .click(LoginPage.btnLogin)
+        await t.expect(ProductsPage.labelProducts.exists).ok()
+        .click(ProductsPage.btnAddToCart)
+        await t.expect(ProductsPage.labelProducts.exists).ok()
+        .click(ProductsPage.imgCart)
+        await t.expect(CartPage.labelProducts.exists).ok()
+})
+
+test('Add  products', async t => {
+    await t
+        .typeText(LoginPage.txtUsername,'standard_user')
+        .typeText(LoginPage.txtPassword,'secret_sauce')
+        .click(LoginPage.btnLogin)
+        await t.expect(ProductsPage.labelProducts.exists).ok()
+        
+
+        const elements = Selector('.btn_primary');
+        var elementsCount = await elements.count;
+        for(let i = 0; i < elementsCount; i++) {
+            const elementSelector = elements.nth(i);
+            console.log(elementSelector)
+                }
+
+})
